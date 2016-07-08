5274e54df9d82fb29b1c8db0e57d25a8f949c8a5
NathanGE <nathangelliott@gmail.com>
2016-07-08 14:22:22 -0400

    Make it possible to view a user's index page and show follow, unfollow buttons

--- shouter/app/models/user.rb
+++ shouter/app/models/user.rb
@@ -12,4 +12,8 @@ class User < ActiveRecord::Base
     foreign_key: :followed_user_id, 
     class_name: 'FollowingRelationship'
   has_many :followers, through: :follower_relationships
+
+  def following? user
+    followed_user_ids.include? user.id
+  end
 end
--- shouter/config/routes.rb
+++ shouter/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root 'homes#show'
   resource :dashboard, only: [:show]
   resource :session, only: [:new, :create, :destroy]
-  resources :users, only: [:new, :create, :show] do
+  resources :users, only: [:index, :new, :create, :show] do
     post 'follow' => 'following_relationships#create'
+    delete 'folow' => 'following_relationships#destroy'
   end
   resources :shouts, only: [:show]
   resources :text_shouts, only: [:create]
